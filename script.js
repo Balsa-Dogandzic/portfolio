0bab14de1ee46cf6153316c7325eef9e63867e1f
Balsa-Dogandzic <100625564+Balsa-Dogandzic@users.noreply.github.com>
2023-06-04 18:17:28 +0200

    Set the new portfolio as default

---
+++ script.js
@@ -0,0 +1,32 @@
+//Navigation collapse button
+let checkBox = document.getElementById("check");
+let collapse = document.getElementById("collapse");
+
+collapse.addEventListener("click", () => {
+  checkBox.checked = false;
+});
+
+//Horizontal scroll menu
+let buttonLeft = document.getElementById("button-left");
+let buttonRight = document.getElementById("button-right");
+let scrollMenu = document.getElementsByClassName("horizontal-scroll")[0];
+let skills = document.getElementsByClassName("skill");
+
+buttonLeft.addEventListener("click", () => {
+  let scrollRange = scrollMenu.scrollWidth / skills.length;
+  scrollMenu.scrollBy(-scrollRange, 0);
+});
+
+buttonRight.addEventListener("click", () => {
+  let scrollRange = scrollMenu.scrollWidth / skills.length;
+  scrollMenu.scrollBy(scrollRange, 0);
+});
+
+setInterval(() => {
+  let scrollRange = scrollMenu.scrollWidth / skills.length;
+  if (scrollMenu.scrollWidth <= skills[0].offsetWidth + scrollMenu.scrollLeft) {
+    scrollMenu.scrollBy(-scrollMenu.scrollLeft, 0);
+  } else {
+    scrollMenu.scrollBy(scrollRange, 0);
+  }
+}, 3500);
